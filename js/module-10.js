d988df4fd508cc07e971c70a09401a685f02fc5b
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-03-23 18:00:47 +0200

    add mod-10

--- js/module-10.js
+++ js/module-10.js
@@ -1,3 +1,7 @@
 const title = document.createElement("h1");
 title.textContent = "Module-10";
 document.body.append(title);
+
+
+const date = new Date("2030-03-16");
+console.log(date); // "Sat Mar 16 2030 00:00:00 GMT+0200"
